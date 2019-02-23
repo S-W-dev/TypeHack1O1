55d6048e3e53b0e7e25a3934194a90bb0a6cb63c
BuildTools <unconfigured@null.spigotmc.org>
2019-02-23 15:55:07 -0700

    Update main.py
    HAHAHAHA

--- Typing hack/py/main.py
+++ Typing hack/py/main.py
@@ -4,45 +4,13 @@ from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-a = 'a'
-b = 'b'
-c = 'c'
-d = 'd'
-e = 'e'
-f = 'f'
-g = 'g'
-h = 'h'
-i = 'i'
-j = 'j'
-k = 'k'
-l = 'l'
-m = 'm'
-n = 'n'
-o = 'o'
-p = 'p'
-q = 'q'
-r = 'r'
-s = 's'
-t = 't'
-u = 'u'
-v = 'v'
-w = 'w'
-x = 'x'
-y = 'y'
-z = 'z'
-SPACE = ' '
-
-def type(key):
-            keyboard.press(str(key))
-            keyboard.release(str(key))
-
-
-def mtype(key, time):
-
-    amount = 0
-    while amount < time:
-        type(key)
-        amount = amount + 1
-
-type(a)
-mtype(b, 3)
+def typeit():
+    sentence = input("Enter what you would like to type: ")
+    sentenceArr = list(sentence)
+    print("Typing in 5 seconds...")
+    sleep(5)
+    for i in range(len(sentenceArr)):
+        keyboard.press(sentenceArr[i])
+        keyboard.release(sentenceArr[i])
+    print("Done!")
+typeit()
