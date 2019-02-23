dccd63dcac3f09051a7f165c8767321b1fe9c71e
Walter Shewmake <25shewmwalj@washk12.org>
2019-02-23 16:22:34 -0700

    sadfasf

--- Typing hack/py/main.py
+++ Typing hack/py/main.py
@@ -5,7 +5,7 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
 def typeit():
-    sentence = input("Enter what you would like to type: ")
+    sentence = input("Enter what you would like to type (-l [lesson number]): ")
     sentenceArr = list(sentence)
     print("Typing in 5 seconds...")
     sleep(5)
@@ -15,4 +15,23 @@ def typeit():
         keyboard.release(sentenceArr[i])
     print("Done!")
     typeit()
-typeit()
+
+
+def typelevel():
+    level = input("Enter the level #: ")
+    file = open('levels/'+str(level)+'.txt', 'r')
+    totype = file.read()
+    sentenceArr = list(totype)
+    print("Typing in 5 seconds...")
+    sleep(5)
+    for i in range(len(sentenceArr)):
+        sleep(1/900)
+        keyboard.press(sentenceArr[i])
+        keyboard.release(sentenceArr[i])
+    print("Done!")
+    typelevel()
+
+typelevel()
+
+
+
