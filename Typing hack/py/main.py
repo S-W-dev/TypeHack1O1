73b96c6e6c49187854fd7166b23c2c255ffcc296
BuildTools <unconfigured@null.spigotmc.org>
2019-02-23 16:36:31 -0700

    Update main.py
    Updated

--- Typing hack/py/main.py
+++ Typing hack/py/main.py
@@ -5,33 +5,40 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
 def typeit():
-    sentence = input("Enter what you would like to type (-l [lesson number]): ")
-    sentenceArr = list(sentence)
-    print("Typing in 5 seconds...")
-    sleep(5)
-    for i in range(len(sentenceArr)):
-        sleep(1/900)
-        keyboard.press(sentenceArr[i])
-        keyboard.release(sentenceArr[i])
-    print("Done!")
-    typeit()
-
+        sentence = input("Enter what you would like to type: ")
+        sentenceArr = list(sentence)
+        print("Typing in 5 seconds...")
+        sleep(5)
+        for i in range(len(sentenceArr)):
+            sleep(1/900)
+            keyboard.press(sentenceArr[i])
+            keyboard.release(sentenceArr[i])
+        print("Done!")
+        Main()
 
 def typelevel():
-    level = input("Enter the level #: ")
-    file = open('levels/'+str(level)+'.txt', 'r')
-    totype = file.read()
-    sentenceArr = list(totype)
-    print("Typing in 5 seconds...")
-    sleep(5)
-    for i in range(len(sentenceArr)):
-        sleep(1/900)
-        keyboard.press(sentenceArr[i])
-        keyboard.release(sentenceArr[i])
-    print("Done!")
-    typelevel()
+        level = input("Enter the level #: ")
+        file = open('levels/'+str(level)+'.txt', 'r')
+        totype = file.read()
+        sentenceArr = list(totype)
+        print("Typing in 5 seconds...")
+        sleep(5)
+        for i in range(len(sentenceArr)):
+            sleep(1/900)
+            keyboard.press(sentenceArr[i])
+            keyboard.release(sentenceArr[i])
+        print("Done!")
+        Main()
 
-typelevel()
+def Main():
+    levelornot = input("Would you like to type a custom thing or a level? (C or L)").lower
+
+    if(levelornot == 'l'):
+        typelevel()
+    else:
+        typeit()
+
+Main()
 
 
 
