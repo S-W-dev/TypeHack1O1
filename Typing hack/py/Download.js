d5337558253a87a9098cab6e21a3202840523e62
BuildTools <unconfigured@null.spigotmc.org>
2019-02-24 18:21:51 -0700

    Create Download.js
    Download.js

---
+++ Typing hack/py/Download.js
@@ -0,0 +1,26 @@
+Button = document.createElement("a");
+Button.id = "Button";
+Button.innerHTML = "Download";
+Button.download="download.txt";
+theText = document.getElementById("cleantext").innerHTML.replace(/\n/g, " ");
+theText = theText.replace(/     /g,"");
+theText = theText.replace(/    /g,"");
+theText = theText.replace(/   /g,"");
+theText = theText.replace(/  /g," ");
+document.body.prepend(Button);
+var textFile = null,
+  makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    // returns a URL you can use as a href
+    return textFile;
+  };
+Button.href=makeTextFile(theText);
